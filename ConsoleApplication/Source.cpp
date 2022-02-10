4484bbb0d9774ba5b75499f7380d0e82cd196940
lmnarka <lnarka09@gmail.com>
2022-02-10 21:56:54 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApplication/Source.cpp
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <omp.h>
+#include <locale.h>
+#include <iostream>
+
+
+using std::cout;
+
+int main()
+{
+	int n; // num of threads
+	scanf("%d", &n);
+
+	srand(time(0));
+
+	omp_set_num_threads(n);
+#pragma omp parallel
+	{
+		double value = (float)rand() / RAND_MAX * (23.0*3.0 - 23.0-3.0) + 23.0-3.0;
+		printf("I am %d thread from %d threads, value %f\n", omp_get_thread_num(), n, value);
+	}
+
+	return 0;
+}
