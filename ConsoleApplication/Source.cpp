a8e4a0c734a84b0c906d70673888d298cd7df4ab
lmnarka <lnarka09@gmail.com>
2022-02-11 11:38:43 +0500

    Wrote a new pseudo-random number generator

--- ConsoleApplication/Source.cpp
+++ ConsoleApplication/Source.cpp
@@ -13,11 +13,10 @@ int main()
 	int n; // num of threads
 	scanf("%d", &n);
 
-	srand(time(0));
-
 	omp_set_num_threads(n);
 #pragma omp parallel
 	{
+		srand(int(time(NULL)) * omp_get_thread_num());
 		double value = (float)rand() / RAND_MAX * (23.0*3.0 - 23.0-3.0) + (23.0-3.0);
 		printf("I am %d thread from %d threads, value %f\n", omp_get_thread_num(), n, value);
 	}
